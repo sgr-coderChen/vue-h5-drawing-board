cbd75d41b1ebb00e3feaa0507f8d9e8aa3232d2f
陈晖 <591369322@qq.com>
2021-12-21 17:15:34 +0800

    feat: 取色器集成demo ，引入font-awesome字体库

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant';
-import 'vant/lib/index.css';
 
-Vue.use(Vant);
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 
