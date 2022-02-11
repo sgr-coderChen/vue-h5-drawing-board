74a1ba54760bb72bcb189d15ca2fe99d7ec0bb5c
陈晖 <591369322@qq.com>
2022-02-11 16:37:15 +0800

    [perf] vue移到本地开发环境，解决生产环境出现vue子依赖的问题

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,4 @@ module.exports = {
         extract: false,  // 将组件里的css打入到umd.js中,不需要额外在安装插件后引入css (注意：公共style文件夹的样式需要在入口文件单独引入 src/index.js)
     },
     productionSourceMap: false, // 生产环境 去除 map文件
-    configureWebpack: (config) => {
-        if (process.env.NODE_ENV === 'production') {
-            config.externals = {
-                'vue': 'Vue',
-            }
-        }
-    }
 }
